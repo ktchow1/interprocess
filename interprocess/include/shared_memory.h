6637ea1133dc61cee4834c9ecad16940d8926961
ktchow1 <ktchow1@yahoo.com.hk>
2025-03-25 10:24:21 +0000

    shared memory 2nd version, not completed

--- interprocess/include/shared_memory.h
+++ interprocess/include/shared_memory.h
@@ -13,26 +13,68 @@ namespace ipc
 {
     struct protocol
     {
-        std::atomic<std::uint32_t> m_ready;
-                
-         
+        std::atomic<std::uint32_t> ready; // modified by both producer and consumer
+        std::uint32_t              producer_pid;
+        std::uint32_t              seqnum;
+        std::uint32_t              message_size;
+        char                       message[4096];
     };
-
 
 
     class shared_memory_producer
     {
     public:
-        explicit shared_memory_producer(const std::string& shared_mem_name) : m_dbg("[shared memory producer]")
+        explicit shared_memory_producer(const std::string& sm_name)
+                                       : m_sm_name("[shared memory producer]")
+                                       , m_fd(::shm_open(sm_name.c_str(), O_RDWR | O_CREAT, S_IRUSR | S_IWUSR))
+                                       , m_dbg(m_sm_name)
         {
+            // **************************** //
+            // *** Create shared memory *** //
+            // **************************** //
+            if (m_fd < 0)
+            {
+                m_dbg.throw_exception("Fail in shm_open");
+            }
+            
+            if (::ftruncate(m_fd, sizeof(protocol)) < 0) // truncate to fixed length
+            {
+                ::close(m_fd);
+                ::shm_unlink(m_sm_name.c_str());
+                m_dbg.throw_exception("Fail in ftruncate");
+            }
+
+            m_protocol = reinterpret_cast<protocol*>(::mmap(NULL, sizeof(protocol), PROT_WRITE, MAP_SHARED, m_fd, 0));
+            if (m_protocol == MAP_FAILED)
+            {
+                ::close(m_fd);
+                ::shm_unlink(m_sm_name.c_str());
+                m_dbg.throw_exception("Fail in mmap");
+            }
+
+
+            // ********************* //
+            // *** Init protocol *** //
+            // ********************* //
+            std::memset(m_protocol, 0, sizeof(protocol));
+            std::uint32_t seqnum = 1000;
         }
 
        ~shared_memory_producer()
         {
+            ::close(m_fd);
+            ::shm_unlink(m_sm_name.c_str());
         }
 
     private:
+        const std::string m_sm_name;
+
+    private:
+        int m_fd;
         debugger m_dbg;
+
+    private:
+        protocol* m_protocol;
     };
 
     
